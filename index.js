f2360ae0928976e7c4ee2f69735659aa44c65fc0
Jonathon <jwbialas@gmail.com>
2022-06-29 10:07:51 -0500

    commit tree 1090213d6fc3fe0a0297ff10be8e52ac983f0ad8

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   const event = document.querySelector('p')
+    event.textContent="This is really cool!"
+  });
+  
